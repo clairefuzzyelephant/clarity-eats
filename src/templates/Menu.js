f10bd9487fca30899767537b624b3910469642af
Claire Cheng <clairecheng88@gmail.com>
2022-07-15 16:26:33 -0700

    finally finished styling the dropdown menu. like fr this time

--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -8,86 +8,47 @@ export default function Menu({searchFunction, clearSearch}) {
     const [location, setLocation] = useState(null)
 
     const options = [ //update this
+        { value: 'all', label: 'all locations'},
         { value: 'korea', label: 'korea' },
         { value: 'canada', label: 'canada' },
         { value: 'taiwan', label: 'taiwan' },
       ];
 
     const customStyles = {
-        menu: (provided, state) => ({
-            ...provided,
-            color: state.selectProps.menuColor,
-            backgroundColor: '#F5F5EE',
-            padding: 10,
-            // paddingTop: 10,
-        }),
         control: (provided, state) => ({
             ...provided,
             borderWidth: 0,
-            paddingBottom: 0,
             boxShadow: 0,
-            marginTop: 0,
             color: '#1B1102',
             backgroundColor: '#F5F5EE',
+            marginTop: 4,
         }),
-        placeholder: (provided, state) => ({
-            ...provided,
-            color: '#1B1102',
-            // borderWidth: 0,
-            // paddingTop: 0,
-            // boxShadow: 0,
-            // marginTop: 0,
-        }),
-        input: (provided, state) => ({
-            ...provided,
-            margin: 0,
-            paddingBottom: 0,
-        }),
-        valueContainer: (provided, state) => ({
-            ...provided, 
-            paddingBottom: 3,
-        }),
-        option: (provided, state) => {
-            console.log(state);
-            const backgroundColor = state.isSelected ? "#E9DCC2" : 
-                state.isFocused ? "#F5F5EE" : state.isActive ? '#F5F5EE' : '#F5F5EE';
-            const color = '#1B1102';
-            return {...provided, backgroundColor, color};
-            // borderBottom: '1px solid pink',
-        },
-        menuList: (provided, state) => ({
-            ...provided, 
-            backgroundColor: '#F5F5EE',
-        }),
-
-        
-        // control: (_, { selectProps: { width }}) => ({
-        //     width: 2 * width
-        // }),
-        
-        // singleValue: (provided, state) => {
-        //     const opacity = state.isDisabled ? 0.5 : 1;
-        //     const transition = 'opacity 300ms';
-        
-        //     return { ...provided, opacity, transition };
-        // }
     }
         
 
     return (
         <div className="menuContainer">
             <div className="menuOptions">
-                <div className="menuAllPosts">
-                    all posts
-                </div>
                 <div>
                 <Select
                     styles={customStyles}
                     className="menuLocationSelect"
-                    classNamePrefix="menuLocationSelect"
-                    // isSearchable={false}
+                    // classNamePrefix="menuLocationSelect"
+                    isSearchable={false}
+                    theme={(theme) => ({
+                        ...theme,
+                        colors: {
+                            ...theme.colors,
+                            text: '#1B1102',
+                            primary25: '#E9DCC2',
+                            primary75: '#E9DCC2',
+                            primary50: '#d6c4a1',
+                            primary: '#CCB995',
+                        }
+                    })}
+                    value={location}
                     placeholder="filter by location"
-                    defaultValue={"filter by location"}
+                    defaultValue={"all locations"}
                     onChange={setLocation}
                     options={options}
                 />
