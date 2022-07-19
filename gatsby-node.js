8e33474dc90a2fc56e4ea25c622cd12a94247c11
Claire Cheng <clairecheng88@gmail.com>
2022-07-18 18:19:11 -0700

    10 posts per page

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,7 +34,7 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     `);
     const posts = result.data.allMarkdownRemark.edges
-    const postsPerPage = 6
+    const postsPerPage = 10
     const numPages = Math.ceil(posts.length / postsPerPage)
     Array.from({ length: numPages }).forEach((_, i) => {
       createPage({
