572a942e740e9ad649234cffc33500ccab5a4051
Claire Cheng <clairecheng88@gmail.com>
2022-08-02 14:39:54 +0800

    trim search query

--- src/templates/index.js
+++ src/templates/index.js
@@ -25,6 +25,7 @@ export default function Home({data, pageContext}) {
     if (!query || !index) {
       return []
     } else {
+      query = query.trim()
       let tempResults = []
       Object.keys(index).forEach(idx => {
         tempResults.push(...index[idx].values.search(query))
